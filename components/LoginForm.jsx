5fb6f79a759f6021650fc4f5cbc966647e8b1b03
a7mdzahw <a7mdzahw@gmail.com>
2021-03-11 13:59:04 +0200

    fix login form

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import router from "next/router";
 import { Modal, Spinner } from "react-bootstrap";
 import { toast } from "react-toastify";
 import { auth } from "../firebase";
